9d4c4d9835ad8418aa0a4fa895e8910a33832cdf
sindhu <sindhu@gmail.com>
2026-01-28 12:01:36 +0530

    added

--- calculater/src/main/java/calculater/EvenOdd.java
+++ calculater/src/main/java/calculater/EvenOdd.java
@@ -1,13 +1,19 @@
 package calculater;
 import java.util.Scanner;
+
 public class EvenOdd {
-   public static void main(String[] args) {
-       Scanner reader = new Scanner(System.in);
-       System.out.print("Enter a number: ");
-       int num = reader.nextInt();
-       if (num % 2 == 0)
-           System.out.println(num + " is even");
-       else
-           System.out.println(num + " is odd");
-   }
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);  // Create Scanner object
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();  // Read integer input
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+
+        reader.close();  // Close the scanner
+    }
 }
+
